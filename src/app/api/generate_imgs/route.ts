60f459df9ba00574300d9a23dbbc2c204b612502
Artem Lukoyanov <lukartoil@gmail.com>
2025-01-16 18:06:03 -0500

    NEW: replicate api connected

---
+++ src/app/api/generate_imgs/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import Replicate from "replicate";
+import { writeFile } from "node:fs/promises";
+import path from 'path';
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+
+    if (!prompt) {
+      return NextResponse.json(
+        { error: "Prompt is required" },
+        { status: 400 }
+      );
+    }
+
+    console.log("Running the model...");
+    const output = await replicate.run(
+      "sundai-club/flux-djenydog:8a68b1465db20fe37fde4d3f9abfc378df33eb69615c97280dc5462fd475b2cc",
+      {
+        input: {
+          prompt: prompt,
+          num_inference_steps: 8,
+          model: "schnell"
+        }
+      }
+    );
+
+    const img_url = String(output);
+    return NextResponse.json({ imageUrl: img_url });
+
+  } catch (error) {
+    console.error('Error generating image:', error);
+    return NextResponse.json(
+      { error: "Failed to generate image" },
+      { status: 500 }
+    );
+  }
+} 
